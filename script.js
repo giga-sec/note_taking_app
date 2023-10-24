da2182b525d56e6b923e1f187145cc8282904f68
gianniscarlo <148776445+gianniscarlo@users.noreply.github.com>
2023-10-24 22:16:21 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 let isUpdate = false, updateId;
 
 addBox.addEventListener("click", () => {
-    popupTitle.innerText = "Add a new Note";
-    addBtn.innerText = "Add Note";
+    popupTitle.innerText = "ADD NOTE";
+    addBtn.innerText = "ADD NOTE";
     popupBox.classList.add("show");
     document.querySelector("body").style.overflow = "hidden";
     if(window.innerWidth > 660) titleTag.focus();
