aa8f7816bcd4445a641127d8156ae20ee73d3e56
ultimatereader <l4cghhdq@anonaddy.me>
2023-11-09 17:10:52 +0800

    Bwisit
    Libuga ani oi atay

--- app.py
+++ app.py
@@ -1,32 +1,148 @@
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from summarizer import summarize
 import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+db = SQLAlchemy(app)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    date = db.Column(db.String(20))
+
+def create_database():
+    """
+    Create a database using the current app context and create all the necessary tables.
+
+    This function does not take any parameters.
+
+    This function does not return any values.
+    """
+    with app.app_context():
+        db.create_all()
+
+@app.route('/notes', methods=['GET'])
+def get_notes():
+    """
+    Retrieves all notes from the database and returns them as a JSON response.
+
+    Parameters:
+        None
+
+    Returns:
+        A JSON response containing all the notes in the database.
+    """
+    notes = Note.query.all()
+    notes_data = []
+    for note in notes:
+        notes_data.append({
+            'id': note.id,
+            'title': note.title,
+            'description': note.description,
+            'date': note.date
+        })
+    return jsonify(notes_data)
+
+@app.route('/notes', methods=['POST'])
+def add_note():
+    """
+    Adds a new note to the database.
+
+    Parameters:
+        None
+
+    Returns:
+        A JSON response containing the ID, title, description, and date of the added note.
+    """
+    print("GET")
+    data = request.get_json()
+    title = data['title']
+    description = data['description']
+    date = data['date']
+    note = Note(title=title, description=description, date=date)
+    db.session.add(note)
+    db.session.commit()
+    return jsonify({
+        'id': note.id,
+        'title': note.title,
+        'description': note.description,
+        'date': note.date
+    })
+
+
+@app.route('/notes/<int:id>', methods=['GET'])
+def get_single_note(id):
+    """
+    Update a note with the given ID.
+
+    Parameters:
+        id (int): The ID of the note to be updated.
+
+    Returns:
+        dict: A dictionary containing the updated note's ID, title, description, and date.
+    """
+    print("GET SINGLE NOTE")
+    if request.method == 'GET':
+        # Handle the GET request to fetch the note contents
+        note = Note.query.get(id)
+        return jsonify({
+            'id': note.id,
+            'title': note.title,
+            'description': note.description,
+            'date': note.date
+        })
+
+
+
+@app.route('/notes/<int:id>', methods=['PUT'])
+def update_note(id):
+    if request.method == 'PUT':
+        # Handle the PUT request to update the note
+        print(f"Received PUT request for note ID:hat {id}")
+        data = request.get_json()
+        print(f"Received data for note ID {id}: {data}")
+        note = Note.query.get(id)
+        if note:
+            note.title = data.get('title', note.title)
+            note.description = data.get('description', note.description)
+            note.date = data.get('date', note.date)
+            db.session.commit()
+            return jsonify({
+                'id': note.id,
+                'title': note.title,
+                'description': note.description,
+                'date': note.date
+            })
+        else:
+            return jsonify({'message': 'Note not found'}), 404
+
+@app.route('/notes/<int:id>', methods=['DELETE'])
+def delete_note(id):
+    """
+    Deletes a note with the given ID.
+
+    Parameters:
+        id (int): The ID of the note to be deleted.
+
+    Returns:
+        dict: A JSON object with a message indicating that the note was deleted successfully.
+    """
+    note = Note.query.get(id)
+    db.session.delete(note)
+    db.session.commit()
+    return jsonify({'message': 'Note deleted successfully'})
 
 
 @app.route("/")
 def index():
-    # Do something with the text, such as save it to a database or send it in an email
     print("Started")
     # Return a response to the client
     return render_template("index.html")
 
 
-@app.route("/add_note", methods=["POST"])
-def add_note():
-
-
-    # Get the text from the textarea tag
-    description = request.json["description"]
-    # Print the text to the console
-
-    summarized_text = summarize(description)
-
-    # Return a response to the AJAX request
-    return jsonify({"summarized_text": summarized_text})
-
-
-
 if __name__ == "__main__":
+    create_database()  # Create the table if it doesn't exist
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,130 +1,138 @@
 const addBox = document.querySelector(".add-box"),
-popupBox = document.querySelector(".popup-box"),
-popupTitle = popupBox.querySelector("header p"),
-closeIcon = popupBox.querySelector("header i"),
-titleTag = popupBox.querySelector("input"),
-descTag = popupBox.querySelector("textarea"),
-addBtn = popupBox.querySelector("button");
+    popupBox = document.querySelector(".popup-box"),
+    popupTitle = popupBox.querySelector("header p"),
+    closeIcon = popupBox.querySelector("header i"),
+    titleTag = popupBox.querySelector("input"),
+    descTag = popupBox.querySelector("textarea"),
+    addBtn = popupBox.querySelector("button");
 
 const months = ["January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"];
-const notes = JSON.parse(localStorage.getItem("notes") || "[]");
-let isUpdate = false, updateId;
+
+document.addEventListener("DOMContentLoaded", () => {
+  showNotes();
+});
 
 addBox.addEventListener("click", () => {
     popupTitle.innerText = "ADD NOTE";
     addBtn.innerText = "ADD";
     popupBox.classList.add("show");
     document.querySelector("body").style.overflow = "hidden";
-    if(window.innerWidth > 660) titleTag.focus();
+    if (window.innerWidth > 660) titleTag.focus();
 });
 
 closeIcon.addEventListener("click", () => {
-    isUpdate = false;
     titleTag.value = descTag.value = "";
     popupBox.classList.remove("show");
     document.querySelector("body").style.overflow = "auto";
 });
 
+
 function showNotes() {
-    if(!notes) return;
-    document.querySelectorAll(".note").forEach(li => li.remove());
-    notes.forEach((note, id) => {
-        let filterDesc = note.description.replaceAll("\n", '<br/>');
-        let liTag = `<li class="note">
-                        <div class="details">
-                            <p>${note.title}</p>
-                            <span>${filterDesc}</span>
-                        </div>
-                        <div class="bottom-content">
-                            <span>${note.date}</span>
-                            <div class="settings">
-                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                                <ul class="menu">
-                                    <li onclick="updateNote(${id}, '${note.title}', '${filterDesc}')"><i class="uil uil-pen"></i>Edit</li>
-                                    <li onclick="deleteNote(${id})"><i class="uil uil-trash"></i>Delete</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </li>`;
-        addBox.insertAdjacentHTML("afterend", liTag);
-    });
+    fetch("/notes")
+        .then(response => response.json())
+        .then(notes => {
+            if (!notes) return;
+            document.querySelectorAll(".note").forEach(li => li.remove());
+            notes.forEach((note, id) => {
+                let filterDesc = note.description.replaceAll("\n", '<br/>');
+                let liTag = `<li class="note">
+                                <div class="details">
+                                    <p>${note.title}</p>
+                                    <span>${filterDesc}</span>
+                                </div>
+                                <div class="bottom-content">
+                                    <span>${note.date}</span>
+                                    <div class="settings">
+                                        <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                        <ul class="menu">
+                                            <li onclick="updateNote(${note.id})"><i class="uil uil-pen"></i>Edit</li>
+                                            <li onclick="deleteNote(${note.id})"><i class="uil uil-trash"></i>Delete</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>`;
+                addBox.insertAdjacentHTML("afterend", liTag);
+            });
+        })
+        .catch(error => {
+            console.error("Error retrieving notes:", error);
+        });
 }
+
 showNotes();
+
+
 
 function showMenu(elem) {
     elem.parentElement.classList.add("show");
     document.addEventListener("click", e => {
-        if(e.target.tagName != "I" || e.target != elem) {
+        if (e.target.tagName != "I" || e.target != elem) {
             elem.parentElement.classList.remove("show");
         }
     });
 }
 
+
+
 function deleteNote(noteId) {
     let confirmDel = confirm("Are you sure you want to delete this note?");
     if(!confirmDel) return;
-    notes.splice(noteId, 1);
-    localStorage.setItem("notes", JSON.stringify(notes));
-    showNotes();
+    fetch(`/notes/${noteId}`, {
+        method: "DELETE"
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.message);
+            showNotes();
+        });
 }
 
-function updateNote(noteId, title, filterDesc) {
-    let description = filterDesc.replaceAll('<br/>', '\r\n');
-    updateId = noteId;
-    isUpdate = true;
-    addBox.click();
-    titleTag.value = title;
-    descTag.value = description;
-    popupTitle.innerText = "Update a Note";
-    addBtn.innerText = "Update Note";
+
+
+
+function updateNote(noteId) {
+    fetch(`/notes/${noteId}`)
+        .then(response => response.json())
+        .then(note => {
+            titleTag.value = note.title;
+            descTag.value = note.description;
+            popupTitle.innerText = "Update a Note";
+            addBtn.innerText = "Update Note";
+            popupBox.classList.add("show");
+            document.querySelector("body").style.overflow = "hidden";
+            if (window.innerWidth > 660) titleTag.focus();
+      });
 }
 
 addBtn.addEventListener("click", e => {
     e.preventDefault();
-    let title = titleTag.value.trim(),
-    description = descTag.value.trim();
+    let title = titleTag.value.trim();
+    let description = descTag.value.trim();
 
-    if(title || description) {
-        let currentDate = new Date(),
-        month = months[currentDate.getMonth()],
-        day = currentDate.getDate(),
-        year = currentDate.getFullYear();
+    if (title || description) {
+        let currentDate = new Date();
+        let month = months[currentDate.getMonth()];
+        let day = currentDate.getDate();
+        let year = currentDate.getFullYear();
 
-        let noteInfo = {title, description, date: `${month} ${day}, ${year}`}
-        if(!isUpdate) {
-            notes.push(noteInfo);
-        } else {
-            isUpdate = false;
-            notes[updateId] = noteInfo;
-        }
-        localStorage.setItem("notes", JSON.stringify(notes));
-        showNotes();
-        closeIcon.click();
+        let data = {
+            title: title,
+            description: description,
+            date: `${month} ${day}, ${year}`
+        };
+        fetch("/notes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(newNote => {
+                console.log(newNote);
+                showNotes();
+                closeIcon.click();
+            });
     }
 });
-
-
-// 11/1/2023 -> Rolan
-// Use Ajax to make an asynchronous request to the Flask server.
-// allows to update the index.html page without having to reload the entire page
-// if you have a form on your page and you want to save the form data to the database without reloading the page, you would use AJAX to send the form data to Flask. Flask would then save the form data to the database and send back a response to AJAX. AJAX would then update the user interface with the response from Flask.
-
-function addNote() {
-    var description = document.querySelector("textarea[name='description']").value;
-    console.log(description);
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/add_note");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({description: description}));
-
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        var note = document.querySelector("textarea[name='summarize']");
-        note.value = JSON.parse(xhr.responseText).summarized_text;
-        // The text was successfully sent to the Flask route
-      } else {
-        // An error occurred while sending the text
-      }
-    };
-  }
